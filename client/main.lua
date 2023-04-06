56125e14bd752c3ff8165697b49bacfc69be3270
ikermolinaa19 <113800789+ikermolinaa19@users.noreply.github.com>
2023-04-07 00:13:49 +0200

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,25 @@
+local progress = {}
+
+progress.open = false
+
+Progress = function(message, time, color)
+    if not progress.open then
+        progress.open = true
+        SendNUIMessage({
+            action = 'open',
+            data = {
+                message = message,
+                time = time or 4000,
+                color = color
+            }
+        })
+    end
+end
+
+RegisterNUICallback('finish', function()
+    progress.open = false
+end)
+
+RegisterNetEvent('ms-progressbar:progress', function(message, time, color)
+    Progress(message, time, color)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Molina#6922'
+description 'Redesign of the mdn_progress by Medinaa#7364, this redesign has been done by Molina#6922' 
+
+ui_page 'html/ui.html'
+
+files {
+    'html/*.html',
+    'html/js/*.js',
+    'html/css/*.css'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+exports {
+	'Progress'
+}
